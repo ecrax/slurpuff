ef0940089e509d0a76bf6deb321768b6a6a87e89
Leo Kling <leo.kling.info@gmail.com>
2022-09-14 13:49:03 +0200

    trying out ssr once more

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,19 +37,41 @@ export default withTRPC<AppRouter>({
      */
     const url = `${getBaseUrl()}/api/trpc`;
 
+    const ONE_DAY_SECONDS = 60 * 60 * 24;
+    ctx?.res?.setHeader(
+      "Cache-Control",
+      `s-maxage=1, stale-while-revalidate=${ONE_DAY_SECONDS}`
+    );
+
     return {
       url,
       transformer: superjson,
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      queryClientConfig: {
+        defaultOptions: { queries: { staleTime: 300 } },
+      },
+      headers() {
+        if (ctx?.req) {
+          // To use SSR properly, you need to forward the client's headers to the server
+          // This is so you can pass through things like cookies when we're server-side rendering
+          // If you're using Node 18, omit the "connection" header
+          const { connection: _connection, ...headers } = ctx.req.headers;
+          return {
+            ...headers,
+            // Optional: inform server that it's an SSR request
+            "x-ssr": "1",
+          };
+        }
+        return {};
+      },
     };
   },
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: false,
+  ssr: true,
   //responseMeta({ ctx, clientErrors }) {
   //  if (clientErrors.length) {
   //    // propagate http first error from API calls
